025f89a63ee9a26df366405b2d50156fbb3036f2
chonk <chonk@chonk.net>
2024-09-12 07:18:56 -0700

    move file and formatting

--- Lab2/Lab2/parte1.asm
+++ Lab2/Lab2/parte1.asm
@@ -1,51 +1,51 @@
 ; Empiezo con los vectores de interrupción
 .org 0x0000
-jmp	setup ; dirección de comienzo (vector de reset)
+jmp setup ; dirección de comienzo (vector de reset)
 
-.org 0x001C 
-jmp	_tmr0_int ; salto atención a rutina de comparación A del timer 0
+.org 0x001C
+jmp _tmr0_int ; salto atención a rutina de comparación A del timer 0
 
 
 setup:
 	; PB{2,3,4,5} - son los LEDs del shield
-    ldi	r16, 0b00111101	
+	ldi r16, 0b00111101
 	; Los 4 LEDs son salidas
-	out	DDRB, r16		
+	out DDRB, r16
 	; apago los LEDs
 	ldi r16, 0xFF
 	out PORTB, r16
 
 	; PC{2,3,4} - son los botones del shield
-	ldi	r16, 0b00000000	
+	ldi r16, 0b00000000
 	; 3 botones del shield son entradas
-	out	DDRC, r16
+	out DDRC, r16
 
-	; configuro para que TMR0 cuente hasta OCR0A 
-	; y luego reinicie su valor 
+	; configuro para que TMR0 cuente hasta OCR0A
+	; y luego reinicie su valor
 	; (CTC (Clear Timer on Compare))
-	ldi	r16, 0b00000010
-	out TCCR0A,	r16
+	ldi r16, 0b00000010
+	out TCCR0A, r16
 
 	; prescaler = 1024
-	ldi	r16, 0b00000101
-	out	TCCR0B,	r16
-	
+	ldi r16, 0b00000101
+	out TCCR0B, r16
+
 	; comparo con 125
-	ldi	r16, 124
-	out	OCR0A, r16
-	
+	ldi r16, 124
+	out OCR0A, r16
+
 	; habilito la interrupción del timer (falta global)
-	ldi	r16, 0b00000010
-	sts	TIMSK0,	r16
+	ldi r16, 0b00000010
+	sts TIMSK0, r16
 
 	; Inicializo algunos registros que voy a usar como variables.
 
 	; Utilizaré r24 como contador de segundos
-	ldi	r24, 0x00
+	ldi r24, 0x00
 	; r17 como bandera para los LEDs
 	ldi r17, 0xFF
 	; r18 como contador para 30 segundos
-	ldi r18, 0x00	
+	ldi r18, 0x00
 	sei
 
 start:
