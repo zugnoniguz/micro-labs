45ee4cd7ebc4ed3a75c99ad7390c70d1f0c326b7
Guzman Zugnoni <guzman.zugnoni@correo.ucu.edu.uy>
2024-10-24 19:14:13 -0300

    some more changes

--- Lab5/main.asm
+++ Lab5/main.asm
@@ -31,15 +31,15 @@
 
 ; vector de reset (dirección de comienzo)
 .ORG 0x0000
-jmp	system_init
+jmp system_init
 
 ; vector de cambio para los pines 8..14 (botones)
 .ORG 0x0008
-jmp	_pcint1_int
+jmp _pcint1_int
 
 ; vector de comparación A del timer 0
 .ORG 0x001C
-jmp	_tmr0_int
+jmp _tmr0_int
 
 ; Data segment
 .DSEG
@@ -61,87 +61,89 @@ system_init:
 	;	PD4 es LCHCLK, el reloj para decodificar los datos de los shift registers al display 7seg
 
 	; PB0,2,3,4,5 son salidas
-    ldi	r16, 0b00111101
-	out	DDRB, r16
+	ldi r16, 0b00111101
+	out DDRB, r16
 	; apago los LEDs
-    ldi	r16, 0b00111100
-	out	PORTB, r16
+	ldi r16, 0b00111100
+	out PORTB, r16
 
 	; 3 botones del shield son entradas
-	ldi	r16, 0b00000000
-	out	DDRC, r16
+	ldi r16, 0b00000000
+	out DDRC, r16
 
 	; PD0,4,7 son salidas
-	ldi	r16, 0b10010001
-	out	DDRD, r16
+	ldi r16, 0b10010001
+	out DDRD, r16
 
 	; SCLK a 0
-	cbi	PORTD, 7
+	cbi PORTD, 7
 	; LCHCLK a 0
-	cbi	PORTD, 4
+	cbi PORTD, 4
 
 	; Configuración de interrupción por cambio de estado de botones
-	ldi	r16, 0b00000010
+	ldi r16, 0b00000010
 	; Recepción de interrupciones de PCI1 (PCI8:PCI14)
-	sts	PCICR, r16
-	ldi	r16, 0b00001110
+	sts PCICR, r16
+	ldi r16, 0b00001110
 	; Detectar cambios en (PCI9,PCI10,PCI11)
-	sts	PCMSK1, r16
+	sts PCMSK1, r16
 
 	; Configuración de interrupciones TMR0
 	; Freq = Fclk/(prescaler * (1+OCR0A)
-	ldi	r16, 0b00000010
+	ldi r16, 0b00000010
 	; Cuenta hasta OCR0A y vuelve a cero (Clear Timer on Compare) (cada compare dispara la interrupción).
-	out	TCCR0A, r16
-	ldi	r16, 0b00000100
+	out TCCR0A, r16
+	ldi r16, 0b00000100
 	; prescaler = 256
-	out	TCCR0B, r16
-	ldi	r16, 249
+	out TCCR0B, r16
+	ldi r16, 249
 	; comparo con 249
-	out	OCR0A, r16
-	ldi	r16, 0b00000010
+	out OCR0A, r16
+	ldi r16, 0b00000010
 	; se habilitan interrupciones del timer
-	sts	TIMSK0, r16
+	sts TIMSK0, r16
 
 	; Inicializo USART para transmitir
 	; TODO: Add docs
-	ldi	r16, 0b00001000
-	sts	UCSR0B, r16
-	ldi	r16, 0b00000110
-	sts	UCSR0C, r16
-	ldi	r16, 0x00
-	sts	UBRR0H, r16
-	ldi	r16, 0x67
-	sts	UBRR0L, r16
+	ldi r16, 0b00001000
+	sts UCSR0B, r16
+	ldi r16, 0b00000110
+	sts UCSR0C, r16
+	ldi r16, 0x00
+	sts UBRR0H, r16
+	ldi r16, 0x67
+	sts UBRR0L, r16
 
 	; TODO: This should be a memory location
 	; indica qué digito sale del display (00010000, 00100000, 01000000, 10000000)
-	ldi	r25, 0x10
+	ldi r25, 0x10
 
 	; habilito interrupciones (display y botones)
 	sei
 
 start:
-	ldi	r26, 0x00				;bandera teclado
-	mov	r5,	 r26					;checksum a 0
-	mov	r4,	 r26
+	; bandera teclado
+	ldi r26, 0x00
+	; checksum a 0
+	mov r5, r26
+	mov r4, r26
 
 	rjmp modo_transmisor
 
 
 modo_transmisor:
-	ldi	r16, 0xA3				;semilla de los números seudo-aleatorios (arbitraria)
-	ldi	r17, 0x82
-	ldi	r18, 0xF0
-	ldi	r19, 0x05
+	ldi r16, 0xA3				;semilla de los números seudo-aleatorios (arbitraria)
+	ldi r17, 0x82
+	ldi r18, 0xF0
+	ldi r19, 0x05
 
 modo_transmisor_2:
 	rcall aleatorios					;Genero los números aleatorios (genero un msg_buffer aleatorio)
 	rcall Chksum_512					;Genero Checksum
-	ldi	r26, 0
+	ldi r26, 0
 
 wait_4TX:							;acá me pongo a esperar que alguien presione cualquier botón
-	sbrsr26, 0			    ;Nota: la interrupcion del boton pone r26-bit0 en 1.
+	sbrs r26, 0			    ;Nota: la interrupcion del boton pone r26-bit0 en 1.
 	rjmp wait_4TX
 
 	cli									;deshabilito interrupciones para display y botones
@@ -156,18 +158,18 @@ modo_receptor:
 	ldi		r26,	0
 
 wait_4RX:							;acá me pongo a esperar que alguien presione cualquier botón
-	sbrs	r26,	0			    ;Nota: la interrupcion del boton cambia r26:0.
-	rjmp	wait_4RX
+	sbrs r26, 0			    ;Nota: la interrupcion del boton cambia r26:0.
+	rjmp wait_4RX
 
-;ahora recibo 512 bytes y los dejo en msg_buffer
-	lds		r16,	UDR0			;me aseguro que el buffer esté vacio
-	lds		r16,	UDR0
-	lds		r16,	UDR0
+	; recibo 512 bytes y los dejo en msg_buffer
+	lds r16, UDR0			;me aseguro que el buffer esté vacio
+	lds r16, UDR0
+	lds r16, UDR0
 	cli									;deshabilito interrupciones para disply y botones
-	rcall	RX_512					    ;recibo 512 bytes por poling (es muy ineficiente)
+	rcall RX_512					    ;recibo 512 bytes por poling (es muy ineficiente)
 	sei									;habilito interrupciones para disply y botones
-	rcall	Chksum_512					;calculo el nuevo Cheksum ... debería ser igual al original incluso si introduje errores
-	rjmp	modo_receptor
+	rcall Chksum_512					;calculo el nuevo Cheksum ... debería ser igual al original incluso si introduje errores
+	rjmp modo_receptor
 
 
 
@@ -245,59 +247,59 @@ RX_Wait:
 ; genera 512 bytes pseudo-aleatorios en msg_buffer
 aleatorios:
 	; Y apunta al primer byte del buffer
-	ldi		r28,	low(msg_buffer)
-	ldi		r29,	high(msg_buffer)
+	ldi r28, low(msg_buffer)
+	ldi r29, high(msg_buffer)
 
 aleatorios_loop:
 	; genero un número de 32bits nuevo usando XORSHIFT de 32 bits (https://en.wikipedia.org/wiki/Xorshift)
-	ldi	r20, 13
+	ldi r20, 13
 	call aleatorios_loop_l
-	ldi	r20, 17
+	ldi r20, 17
 	call aleatorios_loop_r
-	ldi	r20, 5
+	ldi r20, 5
 	call aleatorios_loop_l
 
 	; ya se calculó el numero pseudo-aleatorio de 32bits, voy a guardar los 32 bits (podria solo ir guardando de a 8)
 
 	; el número aleatorio lo guardo a partir de adonde apunta el registro Y, voy recorriendo hasta 512
-	st	Y+,	 r16
-	st	Y+,	 r17
-	st	Y+,	 r18
-	st	Y+,	 r19
-	cpi	YL,	 low(msg_buffer_end)
+	st Y+, r16
+	st Y+, r17
+	st Y+, r18
+	st Y+, r19
+	cpi YL, low(msg_buffer_end)
 	brne aleatorios_loop
-	cpi	YH,	 high(msg_buffer_end)
+	cpi YH, high(msg_buffer_end)
 	brne aleatorios_loop
 	ret
 
 aleatorios_loop_l:
-	mov	r0,	 r16
-	mov	r1,	 r17
-	mov	r2,	 r18
-	mov	r3,	 r19
+	mov r0, r16
+	mov r1, r17
+	mov r2, r18
+	mov r3, r19
 ale_rota_l:
 	clc
-	rol	 r0
-	rol	 r1
-	rol	 r2
-	rol	 r3
-	dec	 r20
+	rol r0
+	rol r1
+	rol r2
+	rol r3
+	dec r20
 	brne ale_rota_l
 	rjmp ale_rota_out
 
 aleatorios_loop_r:
-	mov	r0,	 r16
-	mov	r1,	 r17
-	mov	r2,	 r18
-	mov	r3,	 r19
+	mov r0, r16
+	mov r1, r17
+	mov r2, r18
+	mov r3, r19
 
 ale_rota_r:
 	clc
-	ror	 r3
-	ror	 r2
-	ror	 r1
-	ror	 r0
-	dec	 r20
+	ror r3
+	ror r2
+	ror r1
+	ror r0
+	dec r20
 	brne ale_rota_r
 
 ale_rota_out:
@@ -338,39 +340,31 @@ sacanum:
 ;-------------------------------------------------------------------------------------
 
 sacabyte:
-;Voy a sacar un byte por el 7seg
-	ldi		r17, 0x08
+	ldi r17, 0x08
+
 loop_byte1:
-	cbi		PORTD, 7		;SCLK = 0
-	lsr		r16
-	brcs	loop_byte2		;salta si C=1
-	cbi		PORTB, 0		;SD = 0
-	rjmp	loop_byte3
+	; SCLK = 0
+	cbi PORTD, 7
+	lsr r16
+	brcs loop_byte2
+	; Si el LSB de r16 era 0, SD = 0
+	cbi PORTB, 0
+	rjmp loop_byte3
 loop_byte2:
-	sbi		PORTB, 0		;SD = 1
+	; Si el LSB de r16 era 1, SD = 1
+	sbi PORTB, 0
 loop_byte3:
-	sbi		PORTD, 7		;SCLK = 1
-	dec		r17
-	brne	loop_byte1;
+	; Después de escribir a SD, SCLK = 1
+	sbi PORTD, 7
+	dec r17
+	brne loop_byte1
 	ret
 
 segmap:
-.db 0b00000011 ; "0"
-.db 0b10011111 ; "1"
-.db 0b00100101 ; "2"
-.db 0b00001101 ; "3"
-.db 0b10011001 ; "4"
-.db 0b01001001 ; "5"
-.db 0b01000001 ; "6"
-.db 0b00011111 ; "7"
-.db 0b00000001 ; "8"
-.db 0b00001001 ; "9"
-.db 0b00010001 ; "A"
-.db 0b11000001 ; "b"
-.db 0b01100011 ; "C"
-.db 0b10000101 ; "D"
-.db 0b01100001 ; "E"
-.db 0b01110001 ; "F"
+.db	0b00000011, 0b10011111,	0b00100101, 0b00001101 ;"0" "1" "2" "3"
+.db	0b10011001,	0b01001001, 0b01000001,	0b00011111 ;"4" "5" "6" "7"
+.db	0b00000001, 0b00001001, 0b00010001, 0b11000001 ;"8" "9" "A" "b"
+.db	0b01100011, 0b10000101, 0b01100001, 0b01110001 ;"C" "d" "E" "F"
 
 
 ; ------------------------------------------------
